ebe87bfeccc1e437dc69b202d51026f5115aa2dc
Ahmad Faisal <maulidae.rizki@gmail.com>
2025-03-04 18:20:41 +0700

    update CI/CD pt.3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
                             <span>
                                 Read the
                                 <a href="https://laravel.com/docs" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
-                                    <span>Dokumen negara sangat rahasia</span>
+                                    <span>Dokumen negara sangat rahasia part 2</span>
                                     <svg
                                         width="10"
                                         height="11"
